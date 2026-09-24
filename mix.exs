b733b59ac25d8cfd9a453e4e41adc2e6fa86133e
George Fischer <me@george.id.au>
2026-09-24 16:00:06 +1000

    bump version

--- mix.exs
+++ mix.exs
@@ -4,8 +4,8 @@ defmodule Dumbo.MixProject do
   def project do
     [
       app: :dumbo,
-      version: "0.1.0",
-      elixir: "~> 1.20",
+      version: "0.2.0",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       elixirc_paths: elixirc_paths(Mix.env()),
       deps: deps(),
